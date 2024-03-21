b5f77026520ea31c229f5f1ce497118e3e68f66c
thinncrispy <wilson.hayward@gmail.com>
2024-03-21 14:50:16 -0500

    first push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Bannner from "./components/Banner";
+import { useState } from "react";
+import satData from "./components/satData";
+
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  }
   return (
     <div>
-      <Buttons />
+      <Bannner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}  
+      />
       <Table />
     </div>
   );
