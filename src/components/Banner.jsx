5783d6b2ee3e6ba9fda4790b4eac1de01e4d2bf4
thinncrispy <wilson.hayward@gmail.com>
2024-03-21 15:16:47 -0500

    trying again

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import "./styling.css";
 const Banner = () => { 
     return (
         <div>
-            <h1>Orbit  Report</h1>
+            <h1>Orbit Report</h1>
             <p>Click on the buttons to see the satellites in that orbit type</p>
         </div>
     )
